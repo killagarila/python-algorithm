f0ec4f9fec8c2ce2c266edfabd48cdb99450c28f
TH3Eimis <eimantas2.miliauskis@live.uwe.ac.uk>
2023-05-13 17:45:08 +0000

    changes

---
+++ task_2.py
@@ -0,0 +1,19 @@
+oper_fp = "dataset/task1_2_operations.txt"
+nums_fp = "dataset/task1_2_numbers.txt"
+
+
+def main():
+    pass
+
+def oper_sel(oper):
+    if oper == "1":
+        # search
+    elif oper == "2":
+        # insert
+    elif oper == "3":
+        # delete
+    else:
+        return 404
+
+def auto_oper():
+    pass
